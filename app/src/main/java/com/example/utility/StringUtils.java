d49fad519248b7463619dcf134c8c01ee1c1ec43
sahoosunilkumar <sahoosunilkumar@gmail.com>
2019-05-20 09:11:13 +0530

    updating for release 1.1

--- app/src/main/java/com/example/utility/StringUtils.java
+++ app/src/main/java/com/example/utility/StringUtils.java
@@ -1,5 +1,6 @@
 package com.example.utility;
 
 public class StringUtils {
-    public static String GREET = "Hello World7";
+    public static String GREET = "Hello World7-release";
+    public static String BUILD = "release";
 }
